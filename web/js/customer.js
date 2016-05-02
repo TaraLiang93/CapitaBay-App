6ec78862faea0bc1b73add9205aad7057faa6f13
root <root@Tara-ThinkPad-T450>
2016-05-02 19:12:41 -0400

    got sell modal set up for customer but need to finish and connect to the back

--- web/js/customer.js
+++ web/js/customer.js
@@ -6,35 +6,47 @@
 
 $(document).ready(function () {
 
-    $(".coSubmit").click(function () {
-        $.get("/GetConditionalOrderHistory", {"orderID": $("#orderID").val()})
+    $("#gcoSubmit").click(function () {
+        
+        $.get("/ListConditionalOrderHistory", {"orderID": $("#orderID").val()})
                 .done(function (data) {
                     
                     var code = "";
-                    var index = 0;
-                    for(; index<data.history.length; index++){
-                        console.log("yaya")
-                    }
-//                    data = $.parseJSON(data);
-//                    console.log(data);
-//                    $.each(data["history"], function (key, value) {
-//                        console.log("key: " + key + " value: " + value);
-//                        code = "";
-//                        code = "<tr>" +
-//                                "<td>" + value.sharePrice + "</td>" +
-//                                "<td>" + value.stockDate + "</td>" +
-//                                "</tr>";
-//
-//                        $("#condHistory").append(code);
-//
-////                     code
-//                    });
+//                    var index = 0;
+//                    for(; index<data.history.length; index++){
+//                        console.log("yaya")
+//                    }
+                    data = $.parseJSON(''+data+'');
+                    var test = data;
+                    console.log(data);
+                    $.each(data, function (key, value) {
+                        console.log("key: " + key + " value: " + value);
+                        code = "";
+                        code = "<tr>" +
+                                "<td>" + value.sharePrice + "</td>" +
+                                "<td>" + value.stockDate + "</td>" +
+                                "</tr>";
+
+                        $("#condHistory").append(code);
+
+//                     code
+                    });
                 })
-                .fail(function () {
-                    $(".failMessage").val("Cannot get information on this order.");
-                    console.log("sumbiting and getting order history failed");
+                .fail(function (data) {
+//                    $(".failMessage").val("Cannot get information on this order.");
+                    console.log("failed on the server");
                 });
 
+    });
+
+
+
+    $(".saveChanges").each(function(){
+        $(this).click(function(){
+            var parent = $(this).parent().parent();
+            alert("You have click edif for: "+parent.find(".stockSymbol").text());
+            $("#stockModal").modal("show");
+        });
     });
 
     /* ORIGINAL CODE */
